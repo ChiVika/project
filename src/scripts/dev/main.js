b81ccb499a6f2764fb46d1c91efd8278642b072d
ChiVika <animevita03@mail.ru>
2024-04-20 21:03:08 +0700

    popup

--- src/scripts/dev/main.js
+++ src/scripts/dev/main.js
@@ -5,4 +5,53 @@
     navToggle.addEventListener("click", function () {
     root.classList.toggle("show-nav");
     });
+
+    //открытие и закрытие popup
+    const eventPP = document.querySelector("#js-eventPP"); //ищем
+    const eventOpen = document.querySelector("#js-eventOpenBtn")
+    if(eventPP && eventOpen){
+        const closeEventPP = function(event){
+            function close(){
+                document.removeEventListener("keyup", closeEventPP);
+                document.removeEventListener("click", closeEventPP);
+
+                root.classList.remove("show-event-popup");
+            }
+
+            switch (event.type){
+                case "keyup":
+                    if (event.key === "Escape" || event.keyCode == 27) close();
+                    break;
+                case "click":
+                    if (event.target === this || event.target.classList.contains("js-ppCloseBtn")) close();
+                    break;
+            }
+
+        };
+
+        eventOpen.addEventListener("click", function () {
+            root.classList.add("show-event-popup");
+            document.addEventListener("keyup", closeEventPP);
+            eventPP.addEventListener("click", closeEventPP);
+        });
+
+
+        // eventOpen.addEventListener("click", function(){
+        //     root.classList.add("show-event-popup");
+        // });
+
+        // eventPP.addEventListener("click", function(event){
+        //     if (event.target === this || event.target.classList.contains("js-ppCloseBtn")){
+        //         root.classList.remove("show-event-popup");
+        //     }
+        // });
+
+        // document.addEventListener("keyup", function(event){
+        //     if (event.key === "Escape" || event.keyCode == 27){
+        //         root.classList.remove("show-event-popup");
+        //     }
+        // });
+    }
+
 })();
+
